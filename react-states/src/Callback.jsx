047b71c1ef8a74c505eddb2dc096c797ca7d3ebe
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-22 16:30:11 +0530

    initialization

--- react-states/src/Callback.jsx
+++ react-states/src/Callback.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react"
 
+function init(){
+    console.log("init was called");
+    return Math.random()
+}
 
 export default () => {
-    let [count, setCount] = useState(0);
+    let [count, setCount] = useState(init); // if init() -> it will called in each re-rendering but use initial value only
+    console.log("component re-render");
 
     const incCount =  ()=>{
-        // setCount(count+1)  //count by 1
+        setCount(count+1)  //count by 1
         
         
         // setCount(count+2) // count by 2
@@ -15,8 +20,11 @@ export default () => {
         // setCount(count+1)
 
         // alternate to count by 2 yeah now works
-        setCount((count)=>  count+1);
-        setCount(count => count+1);
+        // setCount((count)=>  count+1);
+        // setCount(count => count+1);
+
+        // constant
+        // setCount(25)
 
     }
 
