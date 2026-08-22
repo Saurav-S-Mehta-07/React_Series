0acbfeb0de2afa549f3dad6d7d0e230362106dca
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-22 15:48:13 +0530

    react-states-app created

---
+++ react-states/src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+
+export default () => {
+    
+    let [count, setCount] = useState(0);
+
+    const incCount = async ()=>{
+        console.log(count + " ");
+        await setCount((count)=>count+1);
+        console.log(count + " ")
+    }
+
+    return (
+        <div>
+            <button onClick={incCount}>Count : {count}</button>
+        </div>
+    )
+}
