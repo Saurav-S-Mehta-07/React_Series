2ea15f4b3420780d3db71572782be2cc592ff329
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-22 16:58:21 +0530

    object as a state variable

---
+++ ludo-app/src/LudoBoard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+
+export default () => {
+
+    let [moves, setMoves] = useState({blue : 0, red : 0, yellow : 0, green : 0});
+
+    const incBlue = ()=>{
+       setMoves((prevMoves)=>{
+         return {...prevMoves, blue : prevMoves.blue+1}
+       });
+    }
+     const incGreen = ()=>{
+       setMoves((prevMoves)=>{
+         return {...prevMoves, green : prevMoves.green+1}
+       });
+    }
+     const incYellow = ()=>{
+       setMoves((prevMoves)=>{
+         return {...prevMoves, yellow : prevMoves.yellow+1}
+       });
+    }
+     const incRed = ()=>{
+       setMoves((prevMoves)=>{
+         return {...prevMoves, red : prevMoves.red+1}
+       });
+    }
+
+    return (
+        <div>
+            <p>Game Begins</p>
+            <div className="board">
+                <p>Blue moves = {moves.blue}</p>
+                <button onClick={incBlue} style={{backgroundColor:"blue"}}>+1</button>
+
+                <p>Green moves = {moves.green}</p>
+                <button onClick={incGreen}  style={{backgroundColor:"green"}}>+1</button>
+
+                <p>Yellow moves = {moves.yellow}</p>
+                <button onClick={incYellow}  style={{backgroundColor:"yellow", color:"black"}}>+1</button>
+
+                <p>Red moves = {moves.red}</p>
+                <button onClick={incRed}  style={{backgroundColor:"red"}}>+1</button>
+            </div>
+        </div>
+    )
+}
