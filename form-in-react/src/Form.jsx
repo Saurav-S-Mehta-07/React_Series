0dca324703c6461742f05fc6ad61e1ee528b5785
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-23 14:55:55 +0530

    multiple input

--- form-in-react/src/Form.jsx
+++ form-in-react/src/Form.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
 
 export default () => {
-    let [fullName, setFullName] = useState("")
+    let [fullname, setFullName] = useState("")
+    let [username, setUserName] = useState("")
+
     const handleFullName = (event)=>{
         console.log(event.target.value);
         setFullName(event.target.value);
@@ -10,14 +12,30 @@ export default () => {
     const handleForm = (event)=>{
         event.preventDefault();
         setFullName("");
+        setUserName("");
         console.log("form submitted");
     }
 
+    const handleUserName = (event)=>{
+        setUserName(event.target.value);
+    }
+
+
     return(
         <form action="" onSubmit={handleForm}>
-            <h1>{fullName && fullName}</h1>
+            <h2>{fullname && fullname}</h2>
+            <h3>{username && "@" + username}</h3>
+
+            <label htmlFor="fullname">Fullname : </label>
+            <input type="text" placeholder="enter your full name" id="fullname" value={fullname} onChange={handleFullName}/>
+              <br />
+              <br />
+
             <label htmlFor="username">Username : </label>
-            <input type="text" placeholder="enter your name" id="username" value={fullName} onChange={handleFullName}/>
+            <input type="text" placeholder="enter your username" id="username" value={username} onChange={handleUserName}/>
+            <br />
+              <br />
+
             <button style={{margin:"10px"}}>Submit</button>
         </form>
     )
